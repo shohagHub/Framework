1d516a0221e5c44f06590caf7fd862dba1218a1d
Saleh Enam Shohag <shohag402@gmail.com>
2020-04-23 23:15:10 +0600

    output added.
    Signed-off-by: Saleh Enam Shohag <shohag402@gmail.com>

---
+++ Codes/Output/device/Frame.framework/Headers/Frame.h
@@ -0,0 +1,19 @@
+//
+//  Frame.h
+//  Frame
+//
+//  Created by Saleh Enam Shohag on 18/4/20.
+//  Copyright © 2020 Saleh Enam Shohag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Frame.
+FOUNDATION_EXPORT double FrameVersionNumber;
+
+//! Project version string for Frame.
+FOUNDATION_EXPORT const unsigned char FrameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Frame/PublicHeader.h>
+
+
